02fc371b976a68ea895c5cb1d13a8c4363f75361
Alex Otoo <alexotoous@outlook.com>
2021-09-29 13:20:53 -0400

    blog date, posted by and category done

--- header.php
+++ header.php
@@ -12,11 +12,12 @@
       <div class="site-header__menu group">
         <nav class="main-navigation">
           <ul class="min-list group">
-            <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <li <?php if(is_page("about-us") or wp_get_post_parent_id(0)==5) echo'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
+              <li <?php if(is_page("blog") or wp_get_post_parent_id(0)==16) echo'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
+           
           </ul>
         </nav>
         <div class="site-header__util">
