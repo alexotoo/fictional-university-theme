61c2024691faeb44f018cac8aa483587e92d069e
Alex Otoo <alexotoous@outlook.com>
2021-10-01 12:22:33 -0400

    custom blog post done!

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
-              <li <?php if(is_page("blog") or wp_get_post_parent_id(0)==16) echo'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
+              <li <?php if(get_post_type()=="post") echo'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
            
           </ul>
         </nav>
