370041481c7de25fe69c95ecaa9641ad2121b920
Alex Otoo <alexotoous@outlook.com>
2022-04-18 11:55:15 -0400

    dynamic menu

--- functions.php
+++ functions.php
@@ -11,6 +11,9 @@ function university_files() {
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
+  register_nav_menu( "headerMenuLocation", "Header Menu Location" );
+  register_nav_menu( "footerMenuLocationOne", "footer Location One" );
+  register_nav_menu( "footerMenuLocationTwo", "footer Location Two" );
   add_theme_support('title-tag');
 }
 
--- header.php
+++ header.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes();?>>
   <head>
+  
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
     <header class="site-header">
     <div class="container">
       <h1 class="school-logo-text float-left"><a href="<?php echo site_url('/') ?>"><strong>Fictional</strong> University</a></h1>
@@ -11,14 +16,19 @@
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
-          <ul class="min-list group">
+
+          <!-- output dynamic menu -->
+          <<?php wp_nav_menu(["theme_location" => "headerMenuLocation"]); ?>
+
+          <!-- <ul class="min-list group">
             <li <?php if(is_page("about-us") or wp_get_post_parent_id(0)==5) echo'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
             <li <?php if(get_post_type() == 'event') echo 'class="current-menu-item"'; ?> ><a href="<?php echo get_post_type_archive_link('event') ?>">Events</a></li>
             <li><a href="#">Campuses</a></li>
               <li <?php if(get_post_type()=="post") echo'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
            
-          </ul>
+          </ul> -->
+          
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
@@ -28,3 +38,4 @@
       </div>
     </div>
   </header>
+
